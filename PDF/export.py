4715c2a3f17d7c590e8dff524bd4b6831fae487b
paul <contact@clavierp.net>
2018-11-06 14:54:20 +0100

    Ajout de couleur et de quelques vérifications de status

--- PDF/export.py
+++ PDF/export.py
@@ -5,8 +5,13 @@ import shutil
 import os
 import pathlib
 import hashlib
+import logging
+import io
 
 EXPORT_COMMAND = 'loffice --headless --convert-to pdf "{}"'
+RED = "\033[31m{}\033[0m"
+GREEN = "\033[32m{}\033[0m"
+BLUE = "\033[34m{}\033[0m"
 
 
 def getname(path):
@@ -18,11 +23,19 @@ def getname(path):
 def export(filein, targetdir='.'):
     name = getname(filein)
     shutil.copyfile(filein.as_posix(), targetdir+"/"+name)
-    print("\tCOPY[OK]")
-    subprocess.Popen(EXPORT_COMMAND.format(name), shell=True).wait()
-    print("\tEXPORT[OK]")
+    logging.info("\tCOPY[OK]")
+    p = subprocess.Popen(EXPORT_COMMAND.format(name), shell=True, stdout=subprocess.PIPE)
+    out = p.communicate()[0]
+    p.wait()
+    if(len(out) != 0):
+        logging.info("\tEXPORT[OK]")
+        status = 0
+    else:
+        logging.warning("\tEXPORT[FAIL]: office already started")
+        status = 1
     os.remove(targetdir+'/'+name)
-    print("\tCLEAN[OK]")
+    logging.info("\tCLEAN[OK]")
+    return status
 
 
 def hash(path):
@@ -36,25 +49,32 @@ def processDir(root, cache):
     files = p.glob("**/*.fodt")
     for f in files:
         full_path = f.as_posix()
+        print("{} [    ]".format(full_path), end='', flush=True)
+        status = 0
         fhash = hash(full_path)
         shash = cache.get(full_path, None)
-        print("Exporting {}".format(full_path))
-        print("\tFile Hash: {}".format(fhash))
-        print("\tSaved Hash:{}".format(shash))
+        logging.info("Exporting {}".format(full_path))
+        logging.info("\tFile Hash: {}".format(fhash))
+        logging.info("\tSaved Hash:{}".format(shash))
         if shash != fhash:
-            export(f)
-            cache[full_path] = fhash
+            status = export(f)
+            if status == 0: cache[full_path] = fhash
         else:
-            print("\tSkip")
-        print("")
-
+            logging.info("\tSkip")
+            status = 2
+        if status == 0: print("\b\b\b\b\b"+GREEN.format(" OK ")+"]")
+        if status == 1: print("\b\b\b\b\b"+RED.format("FAIL")+"]")
+        if status == 2: print("\b\b\b\b\b"+BLUE.format("SKIP")+"]")
 
 def getCache(cache_file):
     cache = {}
-    with open(cache_file) as f:
-        for l in f:
-            k, v = l.split(":")
-            cache[k] = v.replace('\n', '')
+    try:
+        with open(cache_file, "r+") as f:
+            for l in f:
+                k, v = l.split(":")
+                cache[k] = v.replace('\n', '')
+    except FileNotFoundError:
+        logging.warning("Cache file not found")
     return cache
 
 
@@ -65,6 +85,7 @@ def saveCache(cache_file, cache):
 
 
 if __name__ == "__main__":
+    logging.basicConfig(filename="export.log", level=logging.DEBUG)
     cache = getCache(".cache")
     processDir("../6", cache)
     processDir("../5", cache)
