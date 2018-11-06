466b6838fe665b5fba2ed119e457d7afa19b6f67
paul <contact@clavierp.net>
2018-11-06 11:16:37 +0100

    Passage à pathlib pour la gestion des chemins

--- PDF/export.py
+++ PDF/export.py
@@ -3,38 +3,36 @@
 import subprocess
 import shutil
 import os
+import pathlib
 
 EXPORT_COMMAND = "loffice --headless --convert-to pdf '{}'"
 
+
 def getname(path):
-    parts = path.split("/")
-    return " - ".join(parts[-3:])
+    pathFull = path.as_posix()
+    parts = pathFull.split('/')
+    return " - ".join(parts[1:])
 
 
 def export(filein, targetdir='.'):
-    print("Processing {}: ".format(filein), end='')
     name = getname(filein)
-    print("new name = {} ".format(name), end='')
-    shutil.copyfile(filein, targetdir+"/"+name)
-    print(" COPY ", end='')
+    shutil.copyfile(filein.as_posix(), targetdir+"/"+name)
+    print("\tCOPY[OK]")
     subprocess.Popen(EXPORT_COMMAND.format(name), shell=True).wait()
-    print(" TRANFORM ", end='')
+    print("\tEXPORT[OK]")
     os.remove(targetdir+'/'+name)
-    print(" REMOVE {} ".format(targetdir+'/'+name))
+    print("\tCLEAN[OK]")
+
 
 def processDir(root):
-    files = os.walk(root)
-    res = []
+    p = pathlib.Path(root)
+    files = p.glob("**/*.fodt")
     for f in files:
-        for g in f[2]:
-            res.append(f[0]+'/'+g)
-    print(res)
-    files = [f for f in res if f[-5:] == ".fodt"]
-
-    for f in files:
+        print("Exporting {}".format(f.as_posix()))
         export(f)
+        print("\n")
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     processDir("../6")
     processDir("../5")
-
